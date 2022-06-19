db59f04501fe9551db117f353a08e9628a49ef32
Antonio Cesar <antoniocss19@gmail.com>
2022-06-19 07:02:02 -0400

    add opção de capitulos

--- web/src/app/leitor/leitor/leitor.component.ts
+++ web/src/app/leitor/leitor/leitor.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild, ViewEncapsulation } from '@angular/core';
 import { LeitorService } from '../leitor.service';
 
 @Component({
@@ -10,10 +10,12 @@ import { LeitorService } from '../leitor.service';
 export class LeitorComponent implements OnInit {
   @ViewChild('elementoAnotacao') elementoAnotacao!: ElementRef;
   pageYOffset = 0;
+  mostrarCapitulos = false;
+  capitulos: any[] = [];
 
   constructor(private adicionarService: LeitorService) {
 
-    var pageYOffset = localStorage.getItem('pageYOffset');
+    let pageYOffset = localStorage.getItem('pageYOffset');
     if (pageYOffset) {
       this.pageYOffset = Number.parseInt(pageYOffset);
     }
@@ -27,20 +29,88 @@ export class LeitorComponent implements OnInit {
 
       this.addSpan(elemento);
 
+      this.createPaginacao(elemento);
+
       for (let i = 0; i < ebook.conteudoArquivosCss.length; i++) {
-        var css = ebook.conteudoArquivosCss[i];
-        var head = document.head || document.getElementsByTagName('head')[0];
-        var style = document.createElement('style');
+        let css = ebook.conteudoArquivosCss[i];
+        let head = document.head || document.getElementsByTagName('head')[0];
+        let style = document.createElement('style');
 
         head.appendChild(style);
         style.appendChild(document.createTextNode(css));
       }
 
       setTimeout(() => {
+        let capitulo = localStorage.getItem('capitulo');
+        this.mostrarCapitulo(capitulo);
         window.scrollTo(0, this.pageYOffset);
       }, 1000);
 
     });
+  }
+
+  createPaginacao(elemento: any) {
+    let tableContents = elemento.querySelectorAll('a[href^="#calibre_link"]');
+
+    for (let i = 0; i < tableContents.length; i++) {
+      let element = tableContents[i];
+      let href = (element.attributes as any).href.value;
+
+      let capituloConteudo = elemento.querySelector(href);
+
+      this.capitulos.push({
+        href: href,
+        capitulo: element.textContent,
+        capituloConteudo: capituloConteudo.innerHTML
+      });
+
+    }
+  }
+  mostrarCapituloAux(href: any) {
+    this.mostrarCapitulo(href);
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 50);
+  }
+
+  mostrarCapitulo(href: any) {
+    const elemento = this.elementoAnotacao.nativeElement;
+    localStorage.setItem('capitulo', href);
+
+    for (let i = 0; i < this.capitulos.length; i++) {
+      let element = this.capitulos[i];
+
+      let capituloConteudo = elemento.querySelector(element.href);
+      if (element.href === href) {
+        capituloConteudo.classList.remove("hide");
+        this.proximoIrmao(capituloConteudo.nextElementSibling, false);
+        continue;
+      }
+
+      capituloConteudo.classList.add("hide");
+      this.proximoIrmao(capituloConteudo.nextElementSibling, true);
+    }
+
+    elemento.querySelector('.calibreMeta')?.classList.add("hide");
+    elemento.querySelector('.calibreEbookContent')?.classList.add("hide");
+    elemento.querySelector('#calibre_link-0')?.classList.add("hide");
+    elemento.querySelector('#calibre_link-4')?.classList.add("hide");
+    elemento.querySelector('#calibre_link-13')?.classList.add("hide");
+
+    this.mostrarCapitulos = false;
+  }
+
+  proximoIrmao(nextElementSibling: any, esconder: boolean): boolean {
+    if (nextElementSibling?.nodeName === 'SPAN') {
+      return this.proximoIrmao(nextElementSibling.nextElementSibling, esconder);
+    }
+
+    if (esconder) {
+      nextElementSibling?.classList.add("hide");
+    } else {
+      nextElementSibling?.classList.remove("hide");
+    }
+    return true;
   }
 
   salvarHtml() {
@@ -55,26 +125,26 @@ export class LeitorComponent implements OnInit {
   }
 
   addSpan(elemento: any) {
-    var span1 = elemento.querySelector('#span1');
+    let span1 = elemento.querySelector('#s1');
     if (span1) {
       return;
     }
 
-    var todosTextNodes = this.textNodesUnder(elemento);
-    var id = 0;
-    for (var i = 0; i < todosTextNodes.length; i++) {
-      var nodeDoMeio = todosTextNodes[i];
+    let todosTextNodes = this.textNodesUnder(elemento);
+    let id = 0;
+    for (let i = 0; i < todosTextNodes.length; i++) {
+      let nodeDoMeio = todosTextNodes[i];
 
-      var splits = nodeDoMeio.data.split(/(\s+)/);
-      var novoHtml = '';
-      for (var x = 0; x < splits.length; x++) {
+      let splits = nodeDoMeio.data.split(/(\s+)/);
+      let novoHtml = '';
+      for (let x = 0; x < splits.length; x++) {
         id++;
-        novoHtml += '<span id="span' + id + '">' + splits[x] + '</span>';
+        novoHtml += '<span id="s' + id + '">' + splits[x] + '</span>';
       }
 
-      // var novoHtml = nodeDoMeio.data.replace(/\w+/g, "<span>$&</span>");
+      // let novoHtml = nodeDoMeio.data.replace(/\w+/g, "<span>$&</span>");
 
-      var span3 = document.createElement('span');
+      let span3 = document.createElement('span');
       span3.innerHTML = `<span>${novoHtml}</span>`;
 
       nodeDoMeio.parentNode.insertBefore(span3, nodeDoMeio);
@@ -85,7 +155,7 @@ export class LeitorComponent implements OnInit {
   }
 
   textNodesUnder(node: any) {
-    var all: any = [];
+    let all: any = [];
     for (node = node.firstChild; node; node = node.nextSibling) {
       if (node.nodeType == 3) all.push(node);
       else all = all.concat(this.textNodesUnder(node));
@@ -98,70 +168,139 @@ export class LeitorComponent implements OnInit {
     localStorage.setItem('pageYOffset', window.pageYOffset.toString());
   }
 
+  /*
+  @HostListener('window:mouseup', ['$event'])
+  handleKeyboardEventMouseup(ev: KeyboardEvent) {
+    console.log(ev);
+    let palavraMarcada = window.getSelection()?.toString() ?? "";
+    if (!palavraMarcada) {
+      return;
+    }
+
+    let spanId = window.getSelection().focusNode.parentElement;
+
+    let div = this.renderer.createElement('div'); //dynamically create element
+
+    div.classList.add("cores-wrapper");
+
+    div.innerHTML = `
+        <div class="cores">
+          <div class="cor amarelo"></div>
+          <div class="cor azul"></div>
+        </div>`;
+
+    this.renderer.appendChild(spanId, div);
+
+this.renderer.listen(div, 'click', (event) => {
+  console.log("test");
+});
+
+    // setTimeout(() => {
+    //   let thisThis = this;
+    //   let amarelo = document.querySelector('.cores .amarelo') as any;
+    //   amarelo.addEventListener('click', function () { thisThis.marcarTexto(); });
+    // }, 1000);
+
+
+  }
+*/
+
   @HostListener('window:mousedown', ['$event'])
   handleKeyboardEvent(ev: KeyboardEvent) {
+    let wrappers = document.querySelectorAll('.cores-wrapper');
+    for (let i = 0; i < wrappers.length; i++) {
+      wrappers[i].parentElement.removeChild(wrappers[i]);
+    }
+
     if (ev.which == 2) {
       ev.preventDefault();
 
       if (ev.which == 2) {
-        var palavraMarcada = window.getSelection()?.toString() ?? "";
-        var palavraClicada = (ev.target as any).textContent;
+        let palavraMarcada = window.getSelection()?.toString() ?? "";
+        let palavraClicada = (ev.target as any).textContent;
 
         if (palavraMarcada) {
-          var audio = new Audio(`https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q=${palavraMarcada}`);
+          let audio = new Audio(`https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q=${palavraMarcada}`);
           audio.play();
           return;
         }
 
-        console.log(ev);
         if (palavraClicada) {
-          var audio = new Audio(`https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q=${palavraClicada}`);
+          let audio = new Audio(`https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q=${palavraClicada}`);
           audio.play();
         }
       }
     }
-
-    if (ev.which == 3) {
-      ev.preventDefault();
-      this.marcarTexto();
-    }
-
   }
 
   marcarTexto(): void {
-    var selection = window.getSelection();
-    var palavra = selection?.toString() ?? "";
+    let selection = window.getSelection();
+    let palavra = selection?.toString() ?? "";
     if (!palavra) {
       return;
     }
 
-    var parentElement = (selection.anchorNode.parentElement.attributes as any);
-    if (parentElement && parentElement['data-pai1']) {
-      var dataPai1 = parentElement['data-pai1'].value;
-      var dataPai2 = parentElement['data-pai2'].value;
+    function selectionIsBackwards() {
+      let sel = window.getSelection();
 
-      this.removerAnotacao(dataPai1, dataPai2);
+      let range = document.createRange();
+      range.setStart(sel.anchorNode, sel.anchorOffset);
+      range.setEnd(sel.focusNode, sel.focusOffset);
 
+      let backwards = range.collapsed;
+      range.detach();
+      return backwards;
     }
+
+    if (selectionIsBackwards()) {
+      let selection = window.getSelection();
+      let range = document.createRange();
+
+      range.setEnd(selection.anchorNode, selection.anchorOffset);
+      range.setStart(selection.focusNode, selection.focusOffset);
+
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+
+    this.removerAnotacaoAux(selection);
 
     this.colocarHighlight(selection);
 
   }
 
+  removerAnotacaoAux(selection: any): void {
+    let parentElement = (selection.anchorNode.parentElement.attributes as any);
+    if (parentElement && parentElement['data-pai1']) {
+      let dataPai1 = parentElement['data-pai1'].value;
+      let dataPai2 = parentElement['data-pai2'].value;
+
+      this.removerAnotacao(dataPai1, dataPai2);
+    }
+
+    parentElement = (selection.focusNode.parentElement.attributes as any);
+    if (parentElement && parentElement['data-pai1']) {
+      let dataPai1 = parentElement['data-pai1'].value;
+      let dataPai2 = parentElement['data-pai2'].value;
+
+      this.removerAnotacao(dataPai1, dataPai2);
+    }
+  }
+
   salvarHighlight(selection: any) {
-    var anchorNodeId = (selection.anchorNode.parentElement.attributes as any).id.value;
-    var anchorOffset = selection.anchorOffset
+    let anchorNodeId = (selection.anchorNode.parentElement.attributes as any).id.value;
+    let anchorOffset = selection.anchorOffset
 
-    var focusNodeId = (selection.focusNode.parentElement.attributes as any).id.value;
-    var focusOffset = selection.focusOffset;
+    let focusNodeId = (selection.focusNode.parentElement.attributes as any).id.value;
+    let focusOffset = selection.focusOffset;
 
-    var bancoString = localStorage.getItem('banco');
-    var banco = JSON.parse(bancoString);
+    let bancoString = localStorage.getItem('banco');
+    let banco = JSON.parse(bancoString);
     if (!banco) {
       banco = { lista: [] };
     }
 
-    var highlight = {
+    let highlight = {
       anchorNodeId: anchorNodeId, anchorOffset: anchorOffset,
       focusNodeId: focusNodeId, focusOffset: focusOffset,
       highlight: selection?.toString()
@@ -174,7 +313,7 @@ export class LeitorComponent implements OnInit {
   }
 
   colocarHighlight(selection: any) {
-    var highlight = this.salvarHighlight(selection);
+    let highlight = this.salvarHighlight(selection);
 
     this.colocarHighlightNodesDoMeio(selection, highlight);
 
@@ -189,16 +328,16 @@ export class LeitorComponent implements OnInit {
   }
 
   colocarHighlightMesmoNode(selection: any, highlight: any) {
-    var texto = (selection.anchorNode as any).wholeText.substring(selection.anchorOffset, selection.focusOffset);
+    let texto = (selection.anchorNode as any).wholeText.substring(selection.anchorOffset, selection.focusOffset);
 
     this.manipularNode(selection.anchorNode, texto, highlight);
   }
 
   colocarHighlightNodeDiferente(selection: any, highlight: any) {
-    var texto01 = (selection.anchorNode as any).wholeText.substring(selection.anchorOffset, (selection.anchorNode as any).wholeText.length);
+    let texto01 = (selection.anchorNode as any).wholeText.substring(selection.anchorOffset, (selection.anchorNode as any).wholeText.length);
     this.manipularNode(selection.anchorNode, texto01, highlight);
 
-    var texto02 = (selection.focusNode as any).wholeText.substring(0, selection.focusOffset);
+    let texto02 = (selection.focusNode as any).wholeText.substring(0, selection.focusOffset);
     this.manipularNode(selection.focusNode, texto02, highlight);
   }
 
@@ -208,7 +347,7 @@ export class LeitorComponent implements OnInit {
         return [];
       }
 
-      var pastStartNode = false, reachedEndNode = false, textNodes: any = [];
+      let pastStartNode = false, reachedEndNode = false, textNodes: any = [];
 
       function getTextNodes(node: any) {
         if (node == startNode) {
@@ -221,7 +360,7 @@ export class LeitorComponent implements OnInit {
             textNodes.push(node);
           }
         } else {
-          for (var i = 0, len = node.childNodes.length; !reachedEndNode && i < len; ++i) {
+          for (let i = 0, len = node.childNodes.length; !reachedEndNode && i < len; ++i) {
             getTextNodes(node.childNodes[i]);
           }
         }
@@ -231,49 +370,45 @@ export class LeitorComponent implements OnInit {
       return textNodes;
     }
 
-    var textNodes = getTextNodesBetween(document.body, selection.anchorNode, selection.focusNode);
+    let textNodes = getTextNodesBetween(document.body, selection.anchorNode, selection.focusNode);
 
-    for (var i = 0; i < textNodes.length; i++) {
-      var nodeDoMeio = textNodes[i];
+    for (let i = 0; i < textNodes.length; i++) {
+      let nodeDoMeio = textNodes[i];
       this.manipularNode(nodeDoMeio, nodeDoMeio.wholeText, highlight);
     }
   }
 
   manipularNode(node: any, texto: any, highlight: any) {
-    var span = document.createElement('span');
-    var idMark = 'm' + ((+new Date) + Math.random() * 100).toString(32).replace('.', '');
-    span.innerHTML = (node as any).wholeText.replace(texto,
-      `<mark id="${idMark}" data-pai1="${highlight.anchorNodeId}" data-pai2="${highlight.focusNodeId}">${texto}</mark>`);
-
-    node.parentNode.insertBefore(span, node);
-    node.parentNode.removeChild(node);
+    node.parentNode.classList.add("mark");
+    node.parentNode.setAttribute("data-pai1", highlight.anchorNodeId);
+    node.parentNode.setAttribute("data-pai2", highlight.focusNodeId);
   }
 
   removerAnotacao(anchorNodeId: any, focusNodeId: any) {
 
     const elemento = this.elementoAnotacao.nativeElement;
-    var pai1 = elemento.querySelector(`#${anchorNodeId} mark`);
-    var pai2 = elemento.querySelector(`#${focusNodeId} mark`);
+    let pai1 = elemento.querySelector(`#${anchorNodeId}`);
+    let pai2 = elemento.querySelector(`#${focusNodeId}`);
 
     function getTextNodesBetween(rootNode: any, startNode: any, endNode: any) {
       if (startNode === endNode) {
         return [];
       }
 
-      var pastStartNode = false, reachedEndNode = false, textNodes: any = [];
+      let pastStartNode = false, reachedEndNode = false, textNodes: any = [startNode, endNode];
 
       function getTextNodes(node: any) {
         if (node == startNode) {
           pastStartNode = true;
         } else if (node == endNode) {
           reachedEndNode = true;
-        } else if (node.nodeType == 3) {
+        } else if (node.className == "mark") {
           //if (pastStartNode && !reachedEndNode && !/^\s*$/.test(node.nodeValue)) {
           if (pastStartNode && !reachedEndNode) {
             textNodes.push(node);
           }
         } else {
-          for (var i = 0, len = node.childNodes.length; !reachedEndNode && i < len; ++i) {
+          for (let i = 0, len = node.childNodes.length; !reachedEndNode && i < len; ++i) {
             getTextNodes(node.childNodes[i]);
           }
         }
@@ -283,41 +418,30 @@ export class LeitorComponent implements OnInit {
       return textNodes;
     }
 
-    function findAncestor(el: any, sel: any) {
-      while ((el = el.parentElement) && !((el.matches || el.matchesSelector).call(el, sel)));
-      return el;
+    let marks = getTextNodesBetween(document.body, pai1, pai2);
+
+    for (let i = 0; i < marks.length; i++) {
+      let mark = marks[i];
+      mark.classList.remove("mark");
+      mark.removeAttribute('data-pai1');
+      mark.removeAttribute('data-pai2');
     }
 
-    var marks = getTextNodesBetween(document.body, pai1, pai2);
-    marks.push(pai1);
-    marks.push(pai2);
 
-    for (var i = 0; i < marks.length; i++) {
-      var mark = marks[i];
 
-      var spanComId = findAncestor(mark, 'span[id^="span"')
-      var textContent = document.createTextNode(spanComId.innerText);
-
-      spanComId.innerHTML = '';
-      spanComId.appendChild(textContent);
-    }
-
-    window.getSelection().removeAllRanges();
     this.salvarHtml();
   }
 
   @HostListener('window:keypress', ['$event'])
   handleKeyboardEventKeypress(event: KeyboardEvent) {
+    if (event.code === 'Space') {
+      event.preventDefault();
+      this.marcarTexto();
+    }
     if (event.code === 'Backquote') {
-      var selection = window.getSelection();
+      let selection = window.getSelection();
 
-      var parentElement = (selection.anchorNode.parentElement.attributes as any);
-      if (parentElement && parentElement['data-pai1']) {
-        var dataPai1 = parentElement['data-pai1'].value;
-        var dataPai2 = parentElement['data-pai2'].value;
-
-        this.removerAnotacao(dataPai1, dataPai2);
-      }
+      this.removerAnotacaoAux(selection);
     }
   }
 }
